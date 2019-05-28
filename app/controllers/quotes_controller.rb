e75db5818662d43b4c33a2fa2359eb974c51d92f
Ricardo Salcido <ricardo.salcido@outlook.com>
2019-05-28 00:55:30 +0000

    lesson 14 end

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -10,7 +10,9 @@ class QuotesController < ApplicationController
 
   def create
    @quote = Quote.create(quote_params)
+
   if @quote.invalid?
+    
     flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
   end
 
